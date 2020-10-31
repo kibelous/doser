03574c500fdcb7c414f355c329cf4e0f1b7d1319
Kyryll Belous <k.belous@Kyrylls-MacBook-Pro.local>
2020-10-31 18:33:48 +0200

    removed old files

--- doser.py
+++ doser.py
@@ -7,9 +7,7 @@ import time
 from multiprocessing import freeze_support, current_process, Process
 import random
 
-# # Define custom modules directory for project
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'custom_modules')))
-# # Import custom modules
+# Import custom modules
 import scapy.all as scapy
 
 
